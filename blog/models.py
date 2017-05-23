278db759a5ac7c0776a5f73562e4736f05439686
Ryan Palo <ryan@thepalos.com>
2017-05-22 20:33:17 -0700

    Create initial blog/post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Blog(models.Model):
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now_add=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
