1efed89f7135a89a77a6cfe4e11222b6c800742d
Ryan Palo <ryan@thepalos.com>
2017-05-22 21:44:43 -0700

    Get initial authentication working (login/logout)

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Blog, Post
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Blog
     template_name = 'blog/detail.html'
     context_object_name = "blog"
 
-class ClassroomHomepage(ListView):
+class ClassroomHomepage(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = "all_posts"
