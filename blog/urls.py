8ab74866e80c0fab3f1abfcd566515fd1a4e7baa
Ryan Palo <ryan@thepalos.com>
2017-05-23 16:20:03 -0700

    Adjust views to reflect new models

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^classrooms/(?P<pk>[0-9]+)/$', views.ClassroomHomepage.as_view(), name='index'),
-    url(r'^blog/(?P<pk>[0-9]+)/$', views.BlogDetailView.as_view(), name='blog-detail'),
+    url(r'^classrooms/(?P<pk>[0-9]+)/$', views.ClassroomHomepage.as_view(), name='classroom-detail'),
+    url(r'^blogs/(?P<pk>[0-9]+)/$', views.BlogDetailView.as_view(), name='blog-detail'),
+    url(r'^profiles/(?P<pk>[0-9]+)/$', views.ProfileView.as_view(), name='profile-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from .models import Blog, Classroom, Post
 
@@ -20,6 +20,17 @@ class ClassroomHomepage(LoginRequiredMixin, DetailView):
         context["posts"] = Post.objects.filter(blog__classroom=self.object).order_by("-date_created")
         return context
 
+class ProfileView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'accounts/student_profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        student = get_object_or_404(User, pk=kwargs["pk"])
+        context["student"] = student
+        context["classlist"] = student.profile.classrooms.all()
+        context["bloglist"] = student.blogs.all()
+        return context
 
 
 
